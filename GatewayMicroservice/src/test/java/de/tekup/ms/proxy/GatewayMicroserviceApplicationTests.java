54b815d23ed6eb337830b9ebefd874a0e5798eb8
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2022-04-25 11:27:19 +0100

    add gateway

---
+++ GatewayMicroservice/src/test/java/de/tekup/ms/proxy/GatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.ms.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
